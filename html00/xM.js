a0af09fd0864dc6ebab6b9fe46a777205ea60b83
Emir Tirso <turumbar451@hotmail.com>
2025-05-12 21:07:28 -0600

    09-10 modulos

---
+++ html00/xM.js
@@ -0,0 +1,11 @@
+import {y} from "./yM.js"
+
+function x(params){
+    y(params)
+    console.log("x: " + params)
+    return"fin x"
+}
+function xx(params){
+    return"fin xx"
+}
+export{x,xx}
---
+++ html00/xyz.js
@@ -0,0 +1,9 @@
+import * as miModulo from './x.js'
+import { y } from './y.js'
+import { z } from './z.js'
+
+document.getElementById("app")
+console.log(x("abc"))
+console.log(xx("def"))
+console.log(y("ghi"))
+console.log(z("jkl"))
